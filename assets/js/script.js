a702fd4a187fd1c9fc074315c26b67141ff5a140
Josh Grube <steamjgrube@gmail.com>
2021-05-18 10:14:03 -0400

    fixed api call

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let containerDiv = document.getElementById("container")
+let searchBtn = document.getElementById("searchBtn")
+let data;
+let apiKey = "&apikey=a9fe69df";
+var userInput = document.querySelector('#userQuery')
+
+function callApi () {
+    var userSearchItem = userInput.value.trim()
+    console.log(userSearchItem)
+    var url = "http://www.omdbapi.com/?s=" + userSearchItem + apiKey
+    console.log(url)
+    $.get(url, function (apiData) {
+        console.log(apiData);
+        var title = apiData.Search[0].Title;
+        var year = apiData.Search[0].Year;
+        var imdburl = "https://www.imdb.com/title/ " + apiData.Search[0].imdbID+"/";
+        var posterimg = "https://www.imdb.com/title/" + apiData.Search[0].Poster
+
+        document.getElementById('result').innerHTML="<h1>"+title+"</h1>"
+        
+    }
+    )}
+searchBtn.addEventListener("click", callApi);
