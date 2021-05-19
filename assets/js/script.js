c6ee5642b1ef32132fb21d9b6ee4929bb5e362d8
Brad Samuel <bradsamuel97@gmail.com>
2021-05-19 14:36:01 -0400

    savind changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,19 +18,25 @@ function callApi () {
         var year = apiData.Search[0].Year;
         var imdburl = "https://www.imdb.com/title/ " + apiData.Search[0].imdbID+"/";
         var posterimg = apiData.Search[0].Poster
-        var relatedSearch = apiData.Search[1].Poster;
-        var relatedSearch2 = apiData.Search[2].Poster
-        var relatedSearch3 = apiData.Search[3].Poster
-        var imdburl1 = "https://www.imdb.com/title/" + apiData.Search[1].imdbID+"/";
-        var imdburl2 = "https://www.imdb.com/title/" + apiData.Search[2].imdbID+"/";
-        var imdburl3 = "https://www.imdb.com/title/" + apiData.Search[3].imdbID+"/";
+        var relatedSearch = apiData.Search[0].Poster;
+        // var relatedSearch2 = apiData.Search[2].Poster
+        // var relatedSearch3 = apiData.Search[3].Poster
+        // var imdburl1 = "https://www.imdb.com/title/" + apiData.Search[1].imdbID+"/";
+        // var imdburl2 = "https://www.imdb.com/title/" + apiData.Search[2].imdbID+"/";
+        // var imdburl3 = "https://www.imdb.com/title/" + apiData.Search[3].imdbID+"/";
+        for (var i = 0; i < apiData.Search[i].Poster.length && i < imdburl.length; i++) {
+            document.getElementById('relatedsearch').innerHTML="<h1><a href=" +imdburl[i]+"><img id='relatedimg' src="+apiData.Search[i].Poster+"></a></h1>"
+            if(relatedSearch[i] === 3) {
+                break;
+            }
+        }
 
         document.getElementById('movietitle').innerHTML="<h1>"+title+"</h1><br><br>"
         document.getElementById('result').innerHTML="<h2><img src="+posterimg+"></h2>"
         document.getElementById('titleyear').innerHTML="<h1>"+year+"</h1>"
-        document.getElementById('relatedsearch').innerHTML="<h1><a href=" +imdburl1+"><img id='relatedimg' src="+relatedSearch+"></a></h1>"
-        document.getElementById('relatedsearch2').innerHTML="<h1><a href=" +imdburl2+"><img id='relatedimg' src="+relatedSearch2+"></a></h1>"
-        document.getElementById('relatedsearch3').innerHTML="<h1><a href=" +imdburl3+"><img id='relatedimg' src="+relatedSearch3+"></a></h1>"
+        // document.getElementById('relatedsearch').innerHTML="<h1><a href=" +imdburl1+"><img id='relatedimg' src="+relatedSearch+"></a></h1>"
+        // document.getElementById('relatedsearch2').innerHTML="<h1><a href=" +imdburl2+"><img id='relatedimg' src="+relatedSearch2+"></a></h1>"
+        // document.getElementById('relatedsearch3').innerHTML="<h1><a href=" +imdburl3+"><img id='relatedimg' src="+relatedSearch3+"></a></h1>"
         
     })
     $.get(urlPlot, function (apiData) {
@@ -54,5 +60,6 @@ function callApi () {
         
         document.getElementById('actors').innerHTML="<h1>"+actors+"</h1>"
     })
+    return imdbID
 }
 searchBtn.addEventListener("click", callApi);
